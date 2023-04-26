885ce775f59f2d5e61cb1cfe34244d88dc8ca354
jhon-mattos <jhon_mattos@outlook.com>
2023-04-26 11:32:12 -0500

    video 32

--- navigations/Navigation.js
+++ navigations/Navigation.js
@@ -1,23 +1,57 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
+import { Icon } from "react-native-elements/dist/icons/Icon";
 
-import Favorites from "../screens/Favorites";
-import TopRestaurants from "../screens/TopRestaurants";
-import Search from "../screens/Search";
-import Account from "../screens/Account";
 import RestaurantsStack from "./RestaurantsStack";
 import FavoritesStack from "./FavoritesStack";
 import TopRestaurantsStack from "./TopRestaurantsStack";
 import SearchStack from "./SearchStack";
 import AccountStack from "./AccountStack";
+// import { Icon } from "react-native-elements/dist/icons/Icon";
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
+  const screenOptions = (route, color) => {
+    let iconName;
+    switch (route.name) {
+      case "restaurantsStack":
+        iconName = "compass-outline";
+        break;
+      case "favoritesStack":
+        iconName = "heart-outline";
+        break;
+      case "top-restaurantsStack":
+        iconName = "star-outline";
+        break;
+      case "searchStack":
+        iconName = "magnify";
+        break;
+      case "accountStack":
+        iconName = "home-outline";
+        break;
+    }
+
+    return (
+      <Icon type="material-community" name={iconName} size={22} color={color} />
+    );
+  };
+
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        initialRouteName="restaurantsStack"
+        // screenOptions={{
+        //   tabBarInactiveTintColor: "#442484",
+        //   tabBarActiveTintColor: "#a17dc3",
+        // }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color }) => screenOptions(route, color),
+          tabBarInactiveTintColor: "#a17dc3",
+          tabBarActiveTintColor: "#442484",
+        })}
+      >
         <Tab.Screen
           name="restaurantsStack"
           component={RestaurantsStack}
