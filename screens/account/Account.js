f87d71f1bb10313bb1aa752f249a7f7e170efbe6
jhon-mattos <jhon_mattos@outlook.com>
2023-05-01 17:15:21 -0500

    video 35

--- screens/account/Account.js
+++ screens/account/Account.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 
 import firebase from "firebase/app";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
@@ -17,10 +17,6 @@ export default function Account() {
 
     user ? setLogin(true) : setLogin(false);
   }, []);
-
-  // getAuth().onAuthStateChanged((user) => {
-  //   user ? setLogin(true) : setLogin(false);
-  // });
 
   if (login == null) {
     return <Loading isVisible={true} text="Cargando..." />;
--- screens/account/UserGuest.js
+++ screens/account/UserGuest.js
@@ -1,14 +1,57 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Loading from "../../components/Loading";
+import { StyleSheet, Text, ScrollView, Image } from "react-native";
+import { Button } from "react-native-elements";
 
 export default function UserGuest() {
   return (
-    <View>
-      <Text>UserGuest...</Text>
-      {/* <Loading isVisible={true} text="Cargando..." /> */}
-    </View>
+    <ScrollView centerContent style={styles.viewBody}>
+      <Image
+        source={require("../../assets/restaurant-logo.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <Text style={styles.title}>Consulta tu perfil en Restaurants</Text>
+      <Text style={styles.description}>
+        ¿Como describirías tu mejor restaurante? Busca y visuliza los mejores
+        restaurantes de una forma sencilla, vota cuál te ha gustado y comenta
+        cómo ha sido tu experiencia.
+      </Text>
+      <Button
+        title="Ver tu perfil"
+        buttonStyle={styles.button}
+        onPress={() => console.log("Click!!!")}
+      />
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  viewBody: {
+    marginHorizontal: 30,
+  },
+
+  image: {
+    height: 300,
+    width: "100%",
+    marginBottom: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 19,
+    marginVertical: 10,
+    textAlign: "center",
+  },
+
+  description: {
+    textAlign: "justify",
+    marginBottom: 20,
+    color: "#a65273",
+  },
+
+  button: {
+    backgroundColor: "#442484",
+  },
+});
