cd1e8d9a8192357af1211a8c7414f4dc30e0390a
jhon-mattos <jhon_mattos@outlook.com>
2023-05-03 12:46:04 -0500

    video 48

---
+++ components/account/InfoUser.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Text } from "react-native";
+import { View } from "react-native";
+
+export default function InfoUser({ user }) {
+  return (
+    <View>
+      <Text>InfoUser..</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
