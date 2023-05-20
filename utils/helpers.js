2b5aafa62b5956cf4f17995e27713389c51c3d33
jhon-mattos <jhon_mattos@outlook.com>
2023-05-20 16:15:09 -0500

    hasta aqui me quede

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,14 @@
+import * as Permissions from "expo-permissions";
+import * as ImagePicker from "expo-image-picker";
+import { Alert } from "react-native";
+
 export function validateEmail(email) {
   const re =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(email);
 }
+
+export const loadImageFromGallery = async (array) => {
+  const response = { status: false, image: null };
+  return response;
+};
